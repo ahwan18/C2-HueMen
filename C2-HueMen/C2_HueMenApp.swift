f5ed8dbfb0be89aed61662ceac059f6cf9d2bcc7
ahwan <ahmadkurniawanibrahim@gmail.com>
2025-05-08 00:31:23 +0700

    Initial Commit

---
+++ C2-HueMen/C2_HueMenApp.swift
@@ -0,0 +1,32 @@
+//
+//  C2_HueMenApp.swift
+//  C2-HueMen
+//
+//  Created by Ahmad Kurniawan Ibrahim on 08/05/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct C2_HueMenApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
